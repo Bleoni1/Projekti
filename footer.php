775c8d6c09865c9393302adaf5edeb7d904d9447
Bleoni1 <bm67290@ubt-uni.net>
2024-02-01 22:14:52 +0100

    New Footer Page

---
+++ footer.php
@@ -0,0 +1,48 @@
+
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lightgallery.min.css">
+    <link rel="stylesheet" href="stili.css">
+</head>
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3> <i class="fas fa-school"></i> Qerdhja Yjet </h3>
+        <p>Aty ku fëmija juaj do ndihet si një yll i vërtet.</p>
+    </div>
+
+    <div class="box">
+        <h3>quick links</h3>
+        <a href="#"> <i class="fas fa-caret-right"></i> Regjistro fëmijën tuaj</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Portali Prindërve</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Kalendari shkollor</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Menuja e ushqimit</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Lista mjeteve të punës</a>
+    </div>
+
+    <div class="box">
+        <h3>Kategoria</h3>
+        <a href="#"> <i class="fas fa-caret-right"></i> Rreth nesh</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Akademi</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Pranimi</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Lajme & të tjera</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Na kontaktoni</a>
+    </div>
+
+    <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-caret-right"></i> Politika e privatësisë</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Kushtet e përdorimim</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Harta e faqes</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> FAQs</a>
+        <a href="#"> <i class="fas fa-caret-right"></i> Deklarat e Aksesueshmërise</a>
+    </div>
+
+</div>
+
+<div class="credit"> &copy; copyright @ 2023 by <span>UBT</span></div>
+
+</section>
